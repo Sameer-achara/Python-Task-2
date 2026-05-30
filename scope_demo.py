eaf04dac78ec5403a25ad12229a4dce9e9d4f84c
sameer achara <sameerachara894@gmail.com>
2026-05-30 15:19:35 +0530

    Add global and local variable demonstration

---
+++ scope_demo.py
@@ -0,0 +1,9 @@
+company = "OpenAI"   ## Global Variable hai
+
+def employee():
+    employee_name = "Sam"    ## local variable hai
+
+    print("Company:", company)
+    print("Employee:", employee_name)
+
+employee()
